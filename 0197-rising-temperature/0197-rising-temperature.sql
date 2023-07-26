f367e89ba7fab70d67b7557f530c62e13c023fc2
Adishjoshi <67081361+Adishjoshi@users.noreply.github.com>
2023-07-25 20:54:17 -0400

    Time: 930 ms (54.73%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,3 @@
 select w1.id from Weather w1, Weather w2 
-where dateDiff(w1.recordDate,w2.recordDate) = 1 And w1.Temperature> w2.Temperature
+where dateDiff(w1.recordDate,w2.recordDate) = 1 #checks for the previous date
+ And w1.Temperature> w2.Temperature             #checks temp on previous date is low
